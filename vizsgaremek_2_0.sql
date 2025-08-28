da41acaa8c3fd708058d92cafb63d495d34df3ef
Barni429 <magyarbarnus@gmail.com>
2025-08-28 11:19:05 +0200

    adatok feltoltese

--- vizsgaremek_2_0.sql
+++ vizsgaremek_2_0.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 27, 2025 at 01:04 PM
+-- Generation Time: Aug 28, 2025 at 09:18 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -34,7 +34,20 @@ END$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `AddSchool` ()   BEGIN
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `AddVehicle` ()   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AddVehicle` (IN `license_plate` VARCHAR(7), IN `fuel` VARCHAR(20), IN `name` VARCHAR(50), IN `owner_id` INT(11), IN `type_id` INT(11))   BEGIN
+	INSERT INTO `vehicle`(
+        `vehicle`.`license_plate`,
+    	`vehicle`.`fuel`,
+    	`vehicle`.`name`,
+    	`vehicle`.`owner_id`,
+    	`vehicle`.`type_id`)
+     VALUES(
+     	 license_plate,
+         fuel,
+         name,
+         owner_id,
+         type_id
+        );
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `FriendRequest` ()   BEGIN
@@ -109,7 +122,7 @@ CREATE TABLE `driving_lessons` (
   `pick_up_place` varchar(100) NOT NULL,
   `drop_off_place` varchar(100) NOT NULL,
   `category_id` int(10) NOT NULL,
-  `is_paid` tinyint(1) NOT NULL,
+  `is_paid` tinyint(1) NOT NULL DEFAULT '0',
   `payment_method` int(10) NOT NULL,
   `appointment_id` int(10) NOT NULL,
   `end_km` int(7) NOT NULL
@@ -127,6 +140,27 @@ CREATE TABLE `driving_lesson_category` (
   `min_age` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `driving_lesson_category`
+--
+
+INSERT INTO `driving_lesson_category` (`id`, `name`, `min_age`) VALUES
+(1, 'AM', 14),
+(2, 'A1', 16),
+(3, 'A2', 18),
+(4, 'A', 24),
+(5, 'B1', 17),
+(6, 'B', 17),
+(7, 'C1', 18),
+(8, 'C', 18),
+(9, 'D1', 18),
+(10, 'D', 18),
+(11, 'BE', 17),
+(12, 'C1E', 18),
+(13, 'CE', 18),
+(14, 'D1E', 18),
+(15, 'DE', 18);
+
 -- --------------------------------------------------------
 
 --
@@ -136,9 +170,19 @@ CREATE TABLE `driving_lesson_category` (
 CREATE TABLE `instructor` (
   `id` int(11) NOT NULL,
   `user_id` int(10) NOT NULL,
-  `promo_text` longtext NOT NULL,
-  `school_id` int(10) NOT NULL
+  `instructor_name` text NOT NULL,
+  `school_id` int(10) NOT NULL,
+  `promo_text` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `instructor`
+--
+
+INSERT INTO `instructor` (`id`, `user_id`, `instructor_name`, `school_id`, `promo_text`) VALUES
+(2, 3, 'name1', 1, ''),
+(3, 7, 'name2', 1, ''),
+(4, 8, 'name3', 1, '');
 
 -- --------------------------------------------------------
 
@@ -178,6 +222,15 @@ CREATE TABLE `payment_methods` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `payment_methods`
+--
+
+INSERT INTO `payment_methods` (`id`, `name`) VALUES
+(1, 'card'),
+(2, 'cash'),
+(3, 'revolut');
 
 -- --------------------------------------------------------
 
@@ -251,22 +304,6 @@ INSERT INTO `role` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `saved_cards`
---
-
-CREATE TABLE `saved_cards` (
-  `id` int(11) NOT NULL,
-  `user_id` int(10) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `pan` varchar(16) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `cvv_cvc` varchar(3) NOT NULL,
-  `exp_date` varchar(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `school`
 --
 
@@ -280,9 +317,15 @@ CREATE TABLE `school` (
   `address` varchar(100) NOT NULL,
   `promo_text` longtext NOT NULL,
   `banner_img_path` varchar(100) NOT NULL,
-  `opening_details_id` int(10) NOT NULL,
   `administrator_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `school`
+--
+
+INSERT INTO `school` (`id`, `name`, `email`, `phone`, `country`, `town`, `address`, `promo_text`, `banner_img_path`, `administrator_id`) VALUES
+(1, 'suli_name1', 'suli_email1', 'suli_tel1', 'suli_country1', 'suli_town1', 'suli_addy1', 'suli_promo1', 'suli_banner1', 1);
 
 -- --------------------------------------------------------
 
@@ -296,6 +339,15 @@ CREATE TABLE `students` (
   `instructor_id` int(10) NOT NULL,
   `user_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `school_id`, `instructor_id`, `user_id`) VALUES
+(1, 1, 2, 4),
+(2, 1, 3, 5),
+(3, 1, 4, 6);
 
 -- --------------------------------------------------------
 
@@ -313,7 +365,7 @@ CREATE TABLE `users` (
   `gender` varchar(50) NOT NULL,
   `education_qualification` varchar(150) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `role_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT '1',
   `pfp_path` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -322,7 +374,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`, `birth_date`, `gender`, `education_qualification`, `password`, `role_id`, `pfp_path`) VALUES
-(1, 'elonev', 'utonev', 'emailcim', '0630303030', '2000-01-01', 'nembinaris', 'ovoda', '38871925b886b86e9c80e869de1cd67128c29c49cf5927d512afa8ff360976c7', NULL, NULL);
+(1, 'elonev', 'utonev', 'emailcim', '0630303030', '2000-01-01', 'nembinaris', 'ovoda', '38871925b886b86e9c80e869de1cd67128c29c49cf5927d512afa8ff360976c7', NULL, NULL),
+(2, 'Elonev2', 'Utonev2', 'Email2', 'Teloszam2', '2000-01-02', 'gender2', 'eq2', '6a05b9c2f026724d6f8ee83bdca46d8b5de9752600802900df61b6ae87f50260', NULL, NULL),
+(3, 'instructor_fn1', 'instructor_ln1', 'instructor_email1', 'instructor_phone1', '2000-01-03', 'gender1', 'instructor__eq1', 'oktato_pass1', 3, 'instructor_pfp1'),
+(4, 'student_fn1', 'student_ln1', 'student_email1', 'student_phone1', '2000-01-01', 'gender', 'student_eq1', '0f7b162b18a1c966e61a7ff729388ceb85e36112b6e670901bda4d21abb8b142', 2, NULL),
+(5, 'student_fn2', 'student_ln2', 'student_email2', 'student_phone2', '2000-01-01', 'gender', 'student_eq2', 'c0cd37c8058e3378757d0e5e51378721e92a987a296d0e69c365632e0c701d1f', 2, NULL),
+(6, 'student_fn3', 'student_ln3', 'student_email3', 'student_phone3', '2000-01-01', 'gender', 'student_eq3', 'f17becbf178a5ffe74257a596866dd821083793257db537e5dbbdc9325ec177f', 2, NULL),
+(7, 'instructor_fn2', 'instructor_ln2', 'instructor_email2', 'instructor_phone2', '2000-01-01', 'gender', 'intructor_eq2', '69ba1bff07ab778655deb4e6b22ca1262c0de90d40ffbb38d45f57c5f28a89b6', 3, NULL),
+(8, 'instructor_fn3', 'instructor_ln3', 'instructor_email3', 'instructor_phone3', '2000-01-01', 'gender', 'intructor_eq3', '5bd317fb1c75de595d41878198893bdcc3ff7dfc06c8cdd2f209e00e06218521', 3, NULL),
+(9, 'admin', 'admin', 'admin', 'admin', '2000-01-01', 'admin', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 5, NULL),
+(10, 'user', 'user', 'user', 'user', '2000-01-01', 'user', 'user', '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -339,6 +400,15 @@ CREATE TABLE `vehicle` (
   `type_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`id`, `license_plate`, `owner_id`, `fuel`, `name`, `type_id`) VALUES
+(3, 'AAAA000', 2, 'gasoline', 'car_name', 1),
+(4, 'aaa000', 3, 'fuel', 'name', 1),
+(5, 'ABC123', 4, 'Benzin', 'Suzuki Ignis', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -349,6 +419,17 @@ CREATE TABLE `vehicle_types` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `vehicle_types`
+--
+
+INSERT INTO `vehicle_types` (`id`, `name`) VALUES
+(1, 'Auto'),
+(2, 'Robogó'),
+(3, 'Nagy motor'),
+(4, 'Busz'),
+(5, 'Kamion');
 
 --
 -- Indexes for dumped tables
@@ -430,13 +511,6 @@ ALTER TABLE `role`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `saved_cards`
---
-ALTER TABLE `saved_cards`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `sc_user` (`user_id`);
-
---
 -- Indexes for table `school`
 --
 ALTER TABLE `school`
@@ -487,13 +561,13 @@ ALTER TABLE `driving_lessons`
 -- AUTO_INCREMENT for table `driving_lesson_category`
 --
 ALTER TABLE `driving_lesson_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `instructor`
 --
 ALTER TABLE `instructor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `message`
@@ -511,7 +585,7 @@ ALTER TABLE `opening_details`
 -- AUTO_INCREMENT for table `payment_methods`
 --
 ALTER TABLE `payment_methods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `request`
@@ -529,49 +603,43 @@ ALTER TABLE `reserved_appointment`
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `saved_cards`
---
-ALTER TABLE `saved_cards`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `school`
 --
 ALTER TABLE `school`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `vehicle`
 --
 ALTER TABLE `vehicle`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `vehicle_types`
 --
 ALTER TABLE `vehicle_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -628,12 +696,6 @@ ALTER TABLE `review`
   ADD CONSTRAINT `about_1` FOREIGN KEY (`about_id`) REFERENCES `school` (`id`),
   ADD CONSTRAINT `about_2` FOREIGN KEY (`about_id`) REFERENCES `instructor` (`id`),
   ADD CONSTRAINT `author` FOREIGN KEY (`author_id`) REFERENCES `students` (`id`);
-
---
--- Constraints for table `saved_cards`
---
-ALTER TABLE `saved_cards`
-  ADD CONSTRAINT `sc_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `school`
