c2e2ee27205219923bac27ebcc6b235520dd001f
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-09-22 21:24:39 +0200

    cors origin beállitása

--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/SecurityConfig.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/SecurityConfig.java
@@ -1,8 +1,36 @@
 package csapat.DrivingLicenseAppAPI.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));        //A tamogatott origineket adjuk meg
+                        config.setAllowedMethods(Collections.singletonList("*"));                            //A tamogatott http verbeket adjuk meg
+                        config.setAllowCredentials(true);                                                    //A cookiekat fogadjuk
+                        config.setAllowedHeaders(Collections.singletonList("*"));                            //A http headerek adjuk meg
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
