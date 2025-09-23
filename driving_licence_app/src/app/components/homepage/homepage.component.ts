8375a4aed4dd800dd906a3b3a8aef46369ea5358
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2025-09-23 14:01:19 +0200

    homePage alapjának elkészitése

--- driving_licence_app/src/app/components/homepage/homepage.component.ts
+++ driving_licence_app/src/app/components/homepage/homepage.component.ts
@@ -1,16 +1,31 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { UsersService } from '../../services/users.service';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [MatDatepickerModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent implements OnInit{
   private userService = inject(UsersService);
+  view = signal<string>("")
 
   ngOnInit(): void {
-    // let role = this.userService.loggedUser().role;
+    let role = this.userService.loggedUser()!.role.getName;
+
+    if (role === "students"){
+
+    } else if (role === "instructor"){
+
+    } else if (role === "schoolAdmin" || role === "schoolOwner"){
+
+    } else if (role === "admin"){
+
+    }
   }
+
+
+
 }
