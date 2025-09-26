9f11339472f8de1596b37c399fa9d2475f893eb7
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-09-26 09:55:03 +0200

    ValidatorCollection megírása & használata

--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/other/ValidatorCollection.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/other/ValidatorCollection.java
@@ -1,4 +1,60 @@
 package csapat.DrivingLicenseAppAPI.other;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class ValidatorCollection {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+
+    public static boolean emailChecker(String email) {
+        if (email == null || email.length() > 100) {
+            return false;
+        }
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    public static boolean passwordChecker(String password) {
+        if (password.length() < 8 || password.length() > 16) {
+            return false;
+        }
+
+        String specialCharacters = "\"!@#$%^&*()-_=+[]{};:,.?/\"";
+        String numbersText = "1234567890";
+        boolean specialChecker = false;
+        boolean upperCaseChecker = false;
+        boolean lowerCaseChecker = false;
+        boolean initChecker = false;
+
+        for (int i = 0; i < password.trim().length(); i++) {
+            String selectedChar = String.valueOf(password.charAt(i));
+
+            if (numbersText.contains(selectedChar)) {
+                initChecker = true;
+            } else if (specialCharacters.contains(selectedChar)) {
+                specialChecker = true;
+            } else if (selectedChar.equals(selectedChar.toUpperCase())) {
+                upperCaseChecker = true;
+            } else if (selectedChar.equals(selectedChar.toLowerCase())) {
+                lowerCaseChecker = true;
+            }
+        }
+
+        return specialChecker && upperCaseChecker && lowerCaseChecker && initChecker;
+    }
+
+    public static boolean phoneValidator(String phone) {
+        if (phone.length() != 12) {
+            return false;
+        } else if (phone.charAt(0) != '+') {
+            return false;
+        }
+
+        ArrayList<String> supplierIds = new ArrayList<String>(Arrays.asList("30", "20", "70", "50", "31"));
+        if (!supplierIds.contains(phone.substring(3, 5))) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/service/UserService.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/service/UserService.java
@@ -3,6 +3,7 @@ package csapat.DrivingLicenseAppAPI.service;
 
 import csapat.DrivingLicenseAppAPI.entity.Students;
 import csapat.DrivingLicenseAppAPI.entity.User;
+import csapat.DrivingLicenseAppAPI.other.ValidatorCollection;
 import csapat.DrivingLicenseAppAPI.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -18,8 +19,6 @@ import java.util.regex.Pattern;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
-    private static final String SPECIAL = "!@#$%^&*()-_=+[]{};:,.?/";
 
     //Endpointok
     public ResponseEntity<Object> login(String username, String password) {
@@ -33,14 +32,14 @@ public class UserService {
     }
 
     public ResponseEntity<Object> register(User newUser) {
-        if (emailChecker(newUser.getEmail()) && passwordChecker(newUser.getPassword())) {
+        if (ValidatorCollection.emailChecker(newUser.getEmail()) && ValidatorCollection.passwordChecker(newUser.getPassword())) {
             User registeredUser = userRepository.save(newUser);
             return ResponseEntity.ok(registeredUser);
-        } else if (!emailChecker(newUser.getEmail()) && !passwordChecker(newUser.getPassword())) {
+        } else if (!ValidatorCollection.emailChecker(newUser.getEmail()) && !ValidatorCollection.passwordChecker(newUser.getPassword())) {
             return ResponseEntity.status(417).body("InvalidPasswordAndEmail");
-        } else if (!emailChecker(newUser.getEmail())) {
+        } else if (!ValidatorCollection.emailChecker(newUser.getEmail())) {
             return ResponseEntity.status(417).body("InvalidEmail");
-        } else if (!passwordChecker(newUser.getPassword())) {
+        } else if (!ValidatorCollection.passwordChecker(newUser.getPassword())) {
             return ResponseEntity.status(417).body("InvalidPassword");
         }
 
@@ -62,39 +61,5 @@ public class UserService {
 
     //---------------------------------
     //Egyeb
-    public static boolean emailChecker(String email) {
-        if (email == null || email.length() > 100) {
-            return false;
-        }
-        return EMAIL_PATTERN.matcher(email).matches();
-    }
 
-    private static boolean passwordChecker(String password) {
-        if (password.length() < 8 || password.length() > 16) {
-            return false;
-        }
-
-        String specialCharacters = "\"!@#$%^&*()-_=+[]{};:,.?/\"";
-        String numbersText = "1234567890";
-        boolean specialChecker = false;
-        boolean upperCaseChecker = false;
-        boolean lowerCaseChecker = false;
-        boolean initChecker = false;
-
-        for (int i = 0; i < password.trim().length(); i++) {
-            String selectedChar = String.valueOf(password.charAt(i));
-
-            if (numbersText.contains(selectedChar)) {
-                initChecker = true;
-            } else if (specialCharacters.contains(selectedChar)) {
-                specialChecker = true;
-            } else if (selectedChar.equals(selectedChar.toUpperCase())) {
-                upperCaseChecker = true;
-            } else if (selectedChar.equals(selectedChar.toLowerCase())) {
-                lowerCaseChecker = true;
-            }
-        }
-
-        return specialChecker && upperCaseChecker && lowerCaseChecker && initChecker;
-    }
 }
