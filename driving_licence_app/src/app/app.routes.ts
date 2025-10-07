7cf93af2b540df2c66087bf7ed65ece65d441be7
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2025-10-07 10:51:00 +0200

    creating profil-page

--- driving_licence_app/src/app/app.routes.ts
+++ driving_licence_app/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegistartionPageComponent } from './components/registartion-page/registartion-page.component';
 import { PasswordResetComponent } from './components/password-reset/password-reset.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ProfilPageComponent } from './components/profil-page/profil-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,4 +12,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'registartion', component: RegistartionPageComponent },
   { path: 'passwordReset', component: PasswordResetComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'profil', component: ProfilPageComponent },
+
 ];
