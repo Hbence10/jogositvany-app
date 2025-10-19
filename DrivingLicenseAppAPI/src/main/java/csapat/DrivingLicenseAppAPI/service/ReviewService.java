a5c06745b63892443ea6c86c6475ef2be3eff38e
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-10-19 22:41:51 +0200

    review controller functionjeinek a létrehozása

--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/service/ReviewService.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/service/ReviewService.java
@@ -1,8 +1,10 @@
 package csapat.DrivingLicenseAppAPI.service;
 
+import csapat.DrivingLicenseAppAPI.entity.Review;
 import csapat.DrivingLicenseAppAPI.repository.ReviewRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Transactional
@@ -11,4 +13,19 @@ import org.springframework.stereotype.Service;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+
+    //
+    public ResponseEntity<Review> createReview(Review newReview) {
+        return null;
+    }
+
+    //
+    public ResponseEntity<Review> updateReview(Review updatedReview) {
+        return null;
+    }
+
+    //
+    public ResponseEntity<Object> deleteReview(Integer id) {
+        return null;
+    }
 }
