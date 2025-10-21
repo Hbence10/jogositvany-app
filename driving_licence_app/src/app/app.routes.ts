63ec9a5c16961d4e4cb28eedf5d6f61d557eb5fc
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2025-10-21 09:18:44 +0200

    routes and icon

--- driving_licence_app/src/app/app.routes.ts
+++ driving_licence_app/src/app/app.routes.ts
@@ -5,6 +5,14 @@ import { RegistartionPageComponent } from './components/registartion-page/regist
 import { PasswordResetComponent } from './components/password-reset/password-reset.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { ProfilPageComponent } from './components/profil-page/profil-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { ForSchoolsComponent } from './components/for-schools/for-schools.component';
+import { ForInstructorsComponent } from './components/for-instructors/for-instructors.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SchoolRegistrationComponent } from './components/school-registration/school-registration.component';
+import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -14,5 +22,13 @@ export const routes: Routes = [
   { path: 'passwordReset', component: PasswordResetComponent },
   { path: 'calendar', component: CalendarComponent },
   { path: 'profil', component: ProfilPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'forSchools', component: ForSchoolsComponent },
+  { path: 'forInstructors', component: ForInstructorsComponent },
+  { path: 'notFound', component: NotFoundComponent },
+  { path: 'schoolRegistration', component: SchoolRegistrationComponent },
+  { path: 'unauthorized', component: UnauthorizedComponent },
+  { path: 'searchPage', component: SearchPageComponent },
 
 ];
