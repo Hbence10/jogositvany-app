684f33394a7d16a81c6afc34d5d38dfbc7860ae7
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2025-11-04 14:09:32 +0100

    small changes in navbar component

--- driving_licence_app/src/app/app.routes.ts
+++ driving_licence_app/src/app/app.routes.ts
@@ -9,28 +9,26 @@ import { AboutComponent } from './components/about/about.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { ForSchoolsComponent } from './components/for-schools/for-schools.component';
 import { ForInstructorsComponent } from './components/for-instructors/for-instructors.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SchoolRegistrationComponent } from './components/school-registration/school-registration.component';
 import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
 import { SearchPageComponent } from './components/search-page/search-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'registration', component: RegistrationPageComponent },
-  { path: 'passwordReset', component: PasswordResetComponent },
   { path: 'calendar', component: CalendarComponent },
   { path: 'profil', component: ProfilPageComponent },
   { path: 'about', component: AboutComponent },
   { path: 'faq', component: FaqComponent },
-  { path: 'forSchools', component: ForSchoolsComponent },
-  { path: 'forInstructors', component: ForInstructorsComponent },
-  { path: 'notFound', component: NotFoundComponent },
-  { path: 'schoolRegistration', component: SchoolRegistrationComponent },
+  { path: 'for-schools', component: ForSchoolsComponent },
+  { path: 'for-instructors', component: ForInstructorsComponent },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: 'school-registration', component: SchoolRegistrationComponent },
   { path: 'unauthorized', component: UnauthorizedComponent },
-  { path: 'searchPage', component: SearchPageComponent },
-  { path: 'registration', component: RegistrationPageComponent },
+  { path: 'search-page', component: SearchPageComponent },
   { path: 'password-reset', component: PasswordResetComponent },
 
 ];
