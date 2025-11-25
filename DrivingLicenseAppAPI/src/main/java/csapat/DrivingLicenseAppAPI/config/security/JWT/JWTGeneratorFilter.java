cb61d6363d04c250ca1836f9e00182c14ecfbc18
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-11-25 13:10:48 +0100

    jwt tokenhez szükséges dependency-k

--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/JWTGeneratorFilter.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/JWTGeneratorFilter.java
@@ -9,15 +9,20 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class JWTGeneratorFilter extends OncePerRequestFilter {
+    private final String secret = "b59e0f3644da7f6b907671e93401443b80f82e26";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null){
             Environment env = getEnvironment();
+            String secret = env.getProperty("JWT_SECRET", this.secret);
+//            SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 
         }
 
