ba38d1ac8ec544af4e9be1887fe4c8cb404b1d52
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-11-25 12:51:03 +0100

    filterek alapjainak létrehozása

--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/JWTGeneratorFilter.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/JWTGeneratorFilter.java
@@ -4,6 +4,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -12,11 +15,17 @@ public class JWTGeneratorFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null){
+            Environment env = getEnvironment();
 
+        }
+
+        filterChain.doFilter(request, response);
     }
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return false;
+        return !request.getServletPath().equals("/users/login");
     }
 }
--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/JWTValidatorFilter.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/JWTValidatorFilter.java
@@ -4,6 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -12,7 +14,16 @@ public class JWTValidatorFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader("Authorization");
+        if (null != jwt) {
+            try {
+                Environment env = getEnvironment();
+            } catch (Exception e) {
+                throw new BadCredentialsException("Invalid Token received!");
+            }
+        }
 
+        filterChain.doFilter(request, response);
     }
 
     @Override
