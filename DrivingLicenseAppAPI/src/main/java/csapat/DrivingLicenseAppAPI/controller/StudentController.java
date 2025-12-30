370f3e7780b3be93e63d5fc5fe3a5b6e34108a3c
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2025-12-30 13:53:09 +0100

    studentController dokumentálása

--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/controller/StudentController.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/controller/StudentController.java
@@ -4,6 +4,9 @@ import csapat.DrivingLicenseAppAPI.service.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -22,10 +25,18 @@ public class StudentController {
     @Operation(summary = "A tanuló óráiról adatok.", description = "Az adott tanuló óráinak összegzése és azokról információ visszaadása.")
     @Parameter(name = "id", description = "A tanulóhoz tartozó id.", required = true, in = ParameterIn.PATH)
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Sikeres lekérés", useReturnTypeSchema = true),
-            @ApiResponse(responseCode = "404", description = "Nem létező diák adatainak a lekérése."),
-            @ApiResponse(responseCode = "422", description = "Hiányzó parameter vagy requestBody"),
-            @ApiResponse(responseCode = "500", description = "A server okozta hiba."),
+            @ApiResponse(responseCode = "200", description = "Sikeres lekérés", content = @Content(
+                    mediaType = "application/json",
+                    schemaProperties = {
+                            @SchemaProperty(name = "drivenLessons", schema = @Schema(implementation = Integer.class)),
+                            @SchemaProperty(name = "plusLessons", schema = @Schema(implementation = Integer.class)),
+                            @SchemaProperty(name = "paidLessons", schema = @Schema(implementation = Integer.class)),
+                            @SchemaProperty(name = "unPaidLessons", schema = @Schema(implementation = Integer.class)),
+                    }
+            )),
+            @ApiResponse(responseCode = "404", description = "Nem létező diák adatainak a lekérése.", content = @Content),
+            @ApiResponse(responseCode = "422", description = "Hiányzó parameter vagy requestBody", content = @Content),
+            @ApiResponse(responseCode = "500", description = "A server okozta hiba.", content = @Content),
     })
     @GetMapping("/lessonDetails/{id}")
     public ResponseEntity<Map<String, Integer>> getLessonDetails(@PathVariable("id") int studentId) {
@@ -35,11 +46,10 @@ public class StudentController {
     @Operation(summary = "Tanuló törlése", description = "Tanuló törlése id alapján")
     @Parameter(name = "id", description = "A tanulóhoz tartozó id.", required = true, in = ParameterIn.PATH)
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Sikeres törlés", useReturnTypeSchema = true),
-            @ApiResponse(responseCode = "404", description = "Nem létező diák törlése."),
-            @ApiResponse(responseCode = "415", description = ""),
-            @ApiResponse(responseCode = "422", description = "Hiányzó parameter vagy requestBody"),
-            @ApiResponse(responseCode = "500", description = "A server okozta hiba."),
+            @ApiResponse(responseCode = "200", description = "Sikeres törlés", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Nem létező diák törlése.", content = @Content),
+            @ApiResponse(responseCode = "422", description = "Hiányzó parameter vagy requestBody", content = @Content),
+            @ApiResponse(responseCode = "500", description = "A server okozta hiba.", content = @Content),
     })
     @DeleteMapping("/{id}")
     private ResponseEntity<Object> deleteStudent(@PathVariable("id") Integer id) {
