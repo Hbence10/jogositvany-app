13b55d309370022ee22a4d8ca6059f4b7c4168fd
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-01-06 10:20:09 +0100

    homepage ts és html szerkesztése

--- driving_licence_app/src/app/components/homepage/homepage.component.ts
+++ driving_licence_app/src/app/components/homepage/homepage.component.ts
@@ -1,15 +1,16 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { UsersService } from '../../services/users.service';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { HomePageUser } from '../../models/notEntity/homepageUser.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';import { HomePageUser } from '../../models/notEntity/homepageUser.model';
 import { StudentService } from '../../services/student.service';
 import { ProfilCardComponent } from '../profil-card/profil-card.component';
 import { RouterLink } from "@angular/router";
+import { ProfileCard } from '../../models/notEntity/profileCard.model';
 
 
 @Component({
   selector: 'app-homepage',
-  imports: [MatDatepickerModule,  RouterLink, ProfilCardComponent],
+  imports: [MatDatepickerModule,  RouterLink, ProfilCardComponent, MatCardModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
@@ -27,5 +28,19 @@ export class HomepageComponent implements OnInit {
         next: response => console.log(response)
       })
     }
+  }
+
+    setProfilCardRows(originList: ProfileCard[]): ProfileCard[][]{
+    const rows: ProfileCard[][] = []
+    for (let i: number = 0; i < originList.length; i+=3){
+      const row: ProfileCard[] = []
+      for (let j = i; j < i+3; j++) {
+        if (originList[j] != undefined) {
+          row.push(originList[j])
+        }
+      }
+      rows.push(row)
+    }
+    return rows;
   }
 }
