f6b62d1b3a6e2c28deda671a5a17069a73022b51
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-01-08 10:43:07 +0100

    request-container ts megírása

--- driving_licence_app/src/app/components/calendar/request-container/request-container.component.ts
+++ driving_licence_app/src/app/components/calendar/request-container/request-container.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { InstructorServiceService } from '../../../services/instructor-service.service';
 import { StudentService } from '../../../services/student.service';
 import { UsersService } from '../../../services/users.service';
 import { DrivingLessonService } from '../../../services/driving-lesson.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DrivingLessonType } from '../../../models/driving-lesson-type.model';
 
 @Component({
   selector: 'app-request-container',
@@ -16,4 +18,26 @@ export class RequestContainerComponent {
   userService = inject(UsersService)
   drivingLessonService = inject(DrivingLessonService)
 
+  drivingLessonTypeList: DrivingLessonType[] = []
+  requestForm!: FormGroup;
+  close = output()
+
+
+  ngOnInit(): void {
+    this.drivingLessonService.getAllDrivingLessonType().subscribe({
+      next: response => this.drivingLessonTypeList = response
+    })
+
+    this.requestForm = new FormGroup({
+      selectedDate: new FormControl("", []),
+      startTime: new FormControl("", []),
+      endTime: new FormControl("", []),
+      selectedType: new FormControl("", []),
+      message: new FormControl("", [])
+    })
+  }
+
+  sendDrivingLessonRequest() {
+    const selectedType = this.drivingLessonTypeList.find(dType => dType.id == this.requestForm.controls["selectedType"].value)
+  }
 }
