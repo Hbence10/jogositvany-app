ee6a9f419d3aac4eead49f734c3eca1013411c4a
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-01-20 11:02:16 +0100

    oninit, lesson select, request metódus megírása

--- driving_licence_app/src/app/components/calendar/calendar.component.ts
+++ driving_licence_app/src/app/components/calendar/calendar.component.ts
@@ -1,20 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { DrivingLessonEditorComponent } from './driving-lesson-editor/driving-lesson-editor.component';
 import { RequestContainerComponent } from './request-container/request-container.component';
 import { DrivingLessonService } from '../../services/driving-lesson.service';
 import { SchoolServiceService } from '../../services/school-service.service';
 import { UsersService } from '../../services/users.service';
 import { DrivingLessons } from '../../models/driving-lessons.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calendar',
-  imports: [RequestContainerComponent, DrivingLessonEditorComponent],
+  imports: [RequestContainerComponent, DrivingLessonEditorComponent, CommonModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit{
   showRequestContainer: boolean = false
-  showEditor: boolean = true
+  showEditor: boolean = false
 
   drivingLessonService = inject(DrivingLessonService)
   userService = inject(UsersService)
@@ -29,9 +30,22 @@ export class CalendarComponent {
     "Szombat",
     "Vasárnap"
   ]
+
   days: { name: string, reservedHours: { startTime: Date, endTime: Date, name: string, drivingLessonId: number }[] }[] = []
   selectedDrivingLesson!: DrivingLessons
   selectedReservedHours: { startTime: Date, endTime: Date, name: string, drivingLessonId: number }[] = []
+
+  ngOnInit(): void {
+    const datesOfWeek: Date[] = this.getWeekDates(this.getWeekOfYear(new Date()), 2026)
+
+    for (let i: number = 0; i < datesOfWeek.length; i++) {
+      this.drivingLessonService.getReservedHourByDate(4, datesOfWeek[i].toISOString().split("T")[0]).subscribe({
+        next: response => {
+          this.days.push({ name: this.dayNames[i], reservedHours: response })
+        }
+      })
+    }
+  }
 
   getWeekDates(week: number, year: number): Date[] {
     const start = new Date(year, 0, 1 + (week - 1) * 7);
@@ -61,7 +75,16 @@ export class CalendarComponent {
     return Math.ceil((dayCount + start.getDay() + 1) / 7);
   }
 
+  selectDrivingLesson(id: number) {
+    this.drivingLessonService.getDrivingLessonById(id).subscribe({
+      next: response => this.selectedDrivingLesson = response,
+      complete: () => this.showEditor = true
+    })
+  }
 
-
+  sendRequest(reservedHours: { startTime: Date, endTime: Date, name: string, drivingLessonId: number }[]) {
+    this.selectedReservedHours = reservedHours
+    this.showRequestContainer = true
+  }
 
 }
