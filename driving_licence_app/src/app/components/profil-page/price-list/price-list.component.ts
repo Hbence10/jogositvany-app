c946ddddd9e0b754c2cafb507b4f5c72f1d82a22
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-01-27 13:22:53 +0100

    price-list és navbar frissítése

--- driving_licence_app/src/app/components/profil-page/price-list/price-list.component.ts
+++ driving_licence_app/src/app/components/profil-page/price-list/price-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { SchoolCategory } from '../../../models/schoolCategory.model';
+import { DrivingLessonService } from '../../../services/driving-lesson.service';
 
 @Component({
   selector: 'app-price-list',
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './price-list.component.html',
   styleUrl: './price-list.component.css'
 })
-export class PriceListComponent {
+export class PriceListComponent implements OnInit {
+  drivingLessonService = inject(DrivingLessonService)
+  schoolId = input.required<number>()
+  priceList: SchoolCategory[] = []
+  close = output()
 
+  ngOnInit(): void {
+    this.drivingLessonService.getDrivingLicenseCategoriesBySchool(this.schoolId()).subscribe({
+      next: response => this.priceList = response
+    })
+  }
 }
