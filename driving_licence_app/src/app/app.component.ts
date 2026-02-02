8683ec0903d0d6e32db747d09c54a9ca95b68b87
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-02-02 12:09:44 +0100

    navbar-phone componens létrehozás, megírása

--- driving_licence_app/src/app/app.component.ts
+++ driving_licence_app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarPhoneComponent } from './components/navbar-phone/navbar-phone.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FooterComponent,NavbarComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent, NavbarPhoneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ driving_licence_app/src/app/components/navbar-phone/navbar-phone.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { Router } from 'express';
+import { UsersService } from '../../services/users.service';
+
+@Component({
+  selector: 'app-navbar-phone',
+  imports: [],
+  templateUrl: './navbar-phone.component.html',
+  styleUrl: './navbar-phone.component.css'
+})
+export class NavbarPhoneComponent {
+    isOpened: boolean = false
+  userService = inject(UsersService)
+  router = inject(Router)
+
+  checkRequestList() {
+    if (this.userService.loggedUser()?.role.name == "ROLE_instructor") {
+      this.router.navigate(["request", "instructor"])
+    } else {
+      this.router.navigate(["request", "school"])
+    }
+  }
+  navigateToStudents() {
+    if (this.userService.loggedUser()?.role.name == "ROLE_instructor") {
+      this.router.navigate(["users", "instructorStudents"])
+    } else {
+      this.router.navigate(["users", "schoolStudent"])
+    }
+  }
+
+  logout() {
+    this.userService.logout();
+  }
+
+  navigate(routerPath: string) {
+    this.isOpened = false
+    this.router.navigate([routerPath])
+  }
+
+  userNavigation() {
+    if (this.userService.loggedUser() == null) {
+      this.router.navigate(["/login"])
+    } else {
+
+    }
+  }
+}
