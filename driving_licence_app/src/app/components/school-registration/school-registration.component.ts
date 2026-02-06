b1a7c2b7ab8e736f3201c9fd2109eb00f3c89192
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-02-06 08:55:00 +0100

    school-registration megírása

--- driving_licence_app/src/app/components/school-registration/school-registration.component.ts
+++ driving_licence_app/src/app/components/school-registration/school-registration.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SchoolServiceService } from '../../services/school-service.service';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-school-registration',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './school-registration.component.html',
   styleUrl: './school-registration.component.css'
 })
-export class SchoolRegistrationComponent {
+export class SchoolRegistrationComponent implements OnInit{
+  close = output()
+  schoolForm!: FormGroup
+  schoolService = inject(SchoolServiceService)
+  userService = inject(UsersService)
+  varmegyek = ["Bács-Kiskun","Baranya","Békés","Borsod-Abaúj-Zemplén","Csongrád-Csanád","Fejér","Győr-Moson-Sopron","Hajdú-Bihar","Heves","Jász-Nagykun-Szolnok","Komárom-Esztergom","Nógrád","Pest","Somogy","Szabolcs-Szatmár-Bereg","Tolna","Vas","Veszprém","Zala"];
 
+  ngOnInit(): void {
+    this.schoolForm = new FormGroup({
+      name: new FormControl("", [Validators.required]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      phone: new FormControl("", [Validators.required]),
+      country: new FormControl("", [Validators.required]),
+      town: new FormControl("", [Validators.required]),
+      address: new FormControl("", [Validators.required]),
+      promoText: new FormControl("", []),
+      ownerId: new FormControl("", [Validators.required])
+    })
+  }
 }
