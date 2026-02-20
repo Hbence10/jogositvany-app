cf324610a580b37016e6305d14fabe8ff0af6b89
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-02-20 19:43:15 +0100

    school-registration component frissítése

--- driving_licence_app/src/app/components/school-registration/school-registration.component.ts
+++ driving_licence_app/src/app/components/school-registration/school-registration.component.ts
@@ -2,7 +2,10 @@ import { Component, inject, OnInit, output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SchoolServiceService } from '../../services/school-service.service';
 import { UsersService } from '../../services/users.service';
+import { School } from '../../models/school.model';
 import { User } from '../../models/user.model';
+import { ProfileCard } from '../../models/notEntity/profileCard.model';
+import { AlertServiceService } from '../../services/alert-service.service';
 
 @Component({
   selector: 'app-school-registration',
@@ -15,6 +18,8 @@ export class SchoolRegistrationComponent implements OnInit {
   schoolForm!: FormGroup
   schoolService = inject(SchoolServiceService)
   userService = inject(UsersService)
+  userList: any[] = []
+  private alertService = inject(AlertServiceService)
   varmegyek = ["Bács-Kiskun", "Baranya", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád-Csanád", "Fejér", "Győr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád", "Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala"];
 
   ngOnInit(): void {
@@ -28,27 +33,31 @@ export class SchoolRegistrationComponent implements OnInit {
       promoText: new FormControl("", []),
       ownerId: new FormControl("", [Validators.required])
     })
+
+    this.userService.getAllUserWithoutPaginator().subscribe({
+      next: response => {
+        this.userList = response
+      }
+    })
   }
 
   createSchool() {
-    let owner!: User
-    this.userService.getUserById(this.schoolForm.controls["ownerId"].value).subscribe({
-      next: response => owner = response,
-      complete: () => {
-        this.schoolService.createSchool({
-          id: null,
-          name:this.schoolForm.controls["name"].value,
-          email:this.schoolForm.controls["email"].value,
-          phone:this.schoolForm.controls["phone"].value,
-          country:this.schoolForm.controls["country"].value,
-          town:this.schoolForm.controls["town"].value,
-          address:this.schoolForm.controls["address"].value,
-          promoText:this.schoolForm.controls["promoText"].value,
-          owner: owner
-        }).subscribe({
-          next: response => console.log(response)
-        })
-
+    this.schoolService.createSchool({
+      schoolName: this.schoolForm.controls["name"].value,
+      email: this.schoolForm.controls["email"].value,
+      phoneNumber: this.schoolForm.controls["phone"].value,
+      county: this.schoolForm.controls["country"].value,
+      town: this.schoolForm.controls["town"].value,
+      address: this.schoolForm.controls["address"].value,
+      promoText: this.schoolForm.controls["promoText"].value,
+      ownerId: this.schoolForm.controls["ownerId"].value
+    }).subscribe({
+      next: response => {
+        this.close.emit()
+      }, error: () => {
+        this.alertService.setAlert("Hiba történt. Próbáld meg újra később!", "error")
+      }, complete: () => {
+        this.alertService.setAlert("Sikeresen létrehoztad az iskolát!", "success")
       }
     })
 
