c0f50351fb5436562b1989f240ecb58bc24cf6e1
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-02-21 14:34:23 +0100

    calendar frissítése

--- driving_licence_app/src/app/components/calendar/calendar.component.ts
+++ driving_licence_app/src/app/components/calendar/calendar.component.ts
@@ -1,16 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { DrivingLessonEditorComponent } from './driving-lesson-editor/driving-lesson-editor.component';
-import { RequestContainerComponent } from './request-container/request-container.component';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { DrivingLessons } from '../../models/driving-lessons.model';
 import { DrivingLessonService } from '../../services/driving-lesson.service';
 import { SchoolServiceService } from '../../services/school-service.service';
 import { UsersService } from '../../services/users.service';
-import { DrivingLessons } from '../../models/driving-lessons.model';
-import { CommonModule } from '@angular/common';
-import { checkPrimeSync } from 'crypto';
+import { DrivingLessonEditorComponent } from './driving-lesson-editor/driving-lesson-editor.component';
+import { RequestContainerComponent } from './request-container/request-container.component';
+import { HourPipe } from '../../pipe/HourPipe';
+import { HourCard } from '../../models/notEntity/hourCard.model';
 
 @Component({
   selector: 'app-calendar',
-  imports: [RequestContainerComponent, DrivingLessonEditorComponent, CommonModule],
+  imports: [RequestContainerComponent, DrivingLessonEditorComponent, CommonModule, HourPipe],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
@@ -23,51 +24,53 @@ export class CalendarComponent implements OnInit {
   schoolService = inject(SchoolServiceService)
   dayNames: string[] = ["Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"]
   shortDayNames: string[] = ["H", "K", "SZE", "CS", "P", "SZO", "V"]
-  days: { name: string, reservedHours: { startTime: Date, endTime: Date, name: string, drivingLessonId: number }[] }[] = []
+  days: { name: string, reservedHours: HourCard[] }[] = []
   selectedDrivingLesson!: DrivingLessons
   selectedReservedHours: { startTime: Date, endTime: Date, name: string, drivingLessonId: number }[] = []
   selectedDate: Date = new Date()
-  datesOfWeek: Date[] = []
+  datesOfWeek = signal<Date[]>([])
   numberOfWeek!: number
   dateForRequest!: Date
   selectedDateInPhoneView: Date = new Date()
   reservedHoursOfSingleDate!: { startTime: Date, endTime: Date, name: string, drivingLessonId: number }[]
 
   ngOnInit(): void {
-    this.numberOfWeek = this.getWeekOfYear(this.selectedDate) - 1
+    this.numberOfWeek = this.getWeekOfYear(this.selectedDate)
     this.getReservedHoursOfWeek(this.numberOfWeek)
     this.getReservedHoursOfSingleDate(this.selectedDateInPhoneView)
   }
 
   getReservedHoursOfSingleDate(wantedDate: Date) {
-    console.log("ASd")
     const formattedDate: string = wantedDate.toLocaleString('hu-HU').replaceAll(". ", "-").substring(0, 10)
     this.drivingLessonService.getReservedHourByDate(this.userService.loggedUser()?.instructorId!, formattedDate).subscribe({
       next: response => {
         this.reservedHoursOfSingleDate = response
-        console.log("----")
-        console.log(response)
-        console.log(this.reservedHoursOfSingleDate)
-        console.log("----")
       }
     })
   }
 
   getReservedHoursOfWeek(numberOfWeek: number) {
     this.days = []
-    this.datesOfWeek = this.getWeekDates(numberOfWeek, 2026)
-    console.log(this.datesOfWeek)
-
-    for (let i: number = 0; i < this.datesOfWeek.length; i++) {
-      const formattedDate: string = this.datesOfWeek[i].toLocaleString('hu-HU').replaceAll(". ", "-").substring(0, 10)
-      console.log(formattedDate)
-      this.drivingLessonService.getReservedHourByDate(this.userService.loggedUser()?.instructorId!, formattedDate).subscribe({
-        next: response => {
-          console.log(response)
-          this.days.push({ name: this.dayNames[i], reservedHours: response })
+    this.datesOfWeek.set(this.getWeekDates(numberOfWeek, 2026))
+    this.drivingLessonService.getReservedHoursBetweenDates(
+      this.userService.loggedUser()?.instructorId!,
+      this.datesOfWeek()[0].toLocaleString('hu-HU').replaceAll(". ", "-").substring(0, 10),
+      this.datesOfWeek()[6].toLocaleString('hu-HU').replaceAll(". ", "-").substring(0, 10),
+    ).subscribe({
+      next: response => {
+        const hourCards: HourCard[] = response
+        for (let i: number = 0; i < 7; i++) {
+          this.days.push({
+            name: this.dayNames[i],
+            reservedHours: hourCards.filter((hc) =>
+              hc.date.toString().split("T")[0] === this.datesOfWeek()[i].toISOString().split("T")[0]
+            )
+          })
         }
-      })
-    }
+      }
+    })
+
+
   }
 
   changeWeekWithArrow(isForward: boolean) {
@@ -75,11 +78,10 @@ export class CalendarComponent implements OnInit {
     this.getReservedHoursOfWeek(this.numberOfWeek)
   }
 
-  changeWeekWithInput(newDate: Date) {
-    this.numberOfWeek = this.getWeekOfYear(newDate) - 1
+  changeWeekWithInput(newDate: string) {
+    this.numberOfWeek = this.getWeekOfYear(new Date(newDate))
     this.getReservedHoursOfWeek(this.numberOfWeek)
   }
-
 
   getWeekDates(week: number, year: number): Date[] {
     const start = new Date(year, 0, 1 + (week - 1) * 7);
@@ -102,11 +104,12 @@ export class CalendarComponent implements OnInit {
   }
 
   getWeekOfYear(date: Date): number {
-    const start = new Date(date.getFullYear(), 0, 1);
-    const diff = date.getTime() - start.getTime();
-
-    const dayCount = diff / 86400000;
-    return Math.ceil((dayCount + start.getDay() + 1) / 7);
+    const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    const dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    const weekNumber = Math.ceil((((d.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
+    return weekNumber;
   }
 
   selectDrivingLesson(id: number) {
@@ -119,7 +122,7 @@ export class CalendarComponent implements OnInit {
   }
 
   sendRequest(dateIndex: number, reservedHours: { startTime: Date, endTime: Date, name: string, drivingLessonId: number }[]) {
-    this.dateForRequest = this.datesOfWeek[dateIndex]
+    this.dateForRequest = this.datesOfWeek()[dateIndex]
     this.selectedReservedHours = reservedHours
     this.showRequestContainer = true
   }
@@ -131,17 +134,17 @@ export class CalendarComponent implements OnInit {
   }
 
   changeDateInPhone(isTommorrow: boolean) {
-    const weeksDate = this.datesOfWeek.map((date) => date.toDateString())
+    const weeksDate = this.datesOfWeek().map((date) => date.toISOString().split("T")[0])
     let nextDate: Date = new Date(this.selectedDateInPhoneView);
-
     nextDate.setDate(nextDate.getDate() + (isTommorrow ? +1 : -1))
     console.log(nextDate)
     if (!weeksDate.includes(nextDate.toDateString())) {
+      this.datesOfWeek.set(this.getWeekDates(this.getWeekOfYear(nextDate), 2026))
       console.log("")
       const nextWeekNumber: number = this.getWeekOfYear(nextDate)
       console.log(nextWeekNumber)
-      this.datesOfWeek = this.getWeekDates(this.getWeekOfYear(nextDate), 2026)
-      console.log(this.datesOfWeek)
+
+      console.log(this.datesOfWeek())
     }
 
     this.getReservedHoursOfSingleDate(nextDate)
