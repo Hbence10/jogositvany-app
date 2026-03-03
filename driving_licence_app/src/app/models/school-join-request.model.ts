e07c9c3b0c154a61b6bfe8d6086783584a7f3eb4
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-03-03 09:21:12 +0100

    school join request model frissítés

--- driving_licence_app/src/app/models/school-join-request.model.ts
+++ driving_licence_app/src/app/models/school-join-request.model.ts
@@ -5,54 +5,13 @@ import { User } from './user.model';
 export class SchoolJoinRequest {
   constructor(
     public id: number,
-    public requestedRole: 'student' | 'instructor',
     public isAccepted: boolean,
-    public accpetedat: Date,
+    public accpetedAt: Date,
     public sentAt: Date,
+    public isDelted: boolean,
+    public deletedAt: Date,
     public schoolJoinRequestUser: User,
-    public schoolJoinRequestShool: School,
+    public schoolJoinRequestSchool: School,
     public joinRequestCategory: DrivingLicenseCategory
   ) {}
-
-  get getRequestedRole(): 'student' | 'instructor' {
-    return this.requestedRole;
-  }
-  set setRequestedRole(value: 'student' | 'instructor') {
-    this.requestedRole = value;
-  }
-
-  get getIsAccepted(): boolean {
-    return this.isAccepted;
-  }
-  set setIsAccepted(value: boolean) {
-    this.isAccepted = value;
-  }
-
-  get getAcceptedAt(): Date {
-    return this.accpetedat;
-  }
-  set setAcceptedAt(value: Date) {
-    this.accpetedat = value;
-  }
-
-  get getSentAt(): Date {
-    return this.sentAt;
-  }
-  set setSentAt(value: Date) {
-    this.sentAt = value;
-  }
-
-  get getSchoolJoinRequestUser(): User {
-    return this.schoolJoinRequestUser;
-  }
-  set setSchoolJoinRequestUser(value: User) {
-    this.schoolJoinRequestUser = value;
-  }
-
-  get getSchoolJoinRequestSchool(): School {
-    return this.schoolJoinRequestShool;
-  }
-  set setSchoolJoinRequestSchool(value: School) {
-    this.schoolJoinRequestShool = value;
-  }
 }
