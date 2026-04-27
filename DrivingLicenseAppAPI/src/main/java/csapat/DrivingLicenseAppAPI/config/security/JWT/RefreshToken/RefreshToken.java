553c7f18cad32534108d029cb050791458c8ddf3
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2026-04-27 21:32:48 +0200

    getterek & setterek #6

--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/RefreshToken/RefreshToken.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/RefreshToken/RefreshToken.java
@@ -4,10 +4,7 @@ import lombok.*;
 
 import java.time.Instant;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
+
 @NoArgsConstructor
 public class RefreshToken {
 
@@ -19,6 +16,48 @@ public class RefreshToken {
     public RefreshToken(String email, String tokenValue, Instant expiredDate) {
         this.email = email;
         this.tokenValue = tokenValue;
+        this.expiredDate = expiredDate;
+    }
+
+    public RefreshToken(Long id, String email, String tokenValue, Instant expiredDate) {
+        this.id = id;
+        this.email = email;
+        this.tokenValue = tokenValue;
+        this.expiredDate = expiredDate;
+    }
+
+    public RefreshToken() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTokenValue() {
+        return tokenValue;
+    }
+
+    public void setTokenValue(String tokenValue) {
+        this.tokenValue = tokenValue;
+    }
+
+    public Instant getExpiredDate() {
+        return expiredDate;
+    }
+
+    public void setExpiredDate(Instant expiredDate) {
         this.expiredDate = expiredDate;
     }
 }
