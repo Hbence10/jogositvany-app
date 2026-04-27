1a9d75e69febf1c6c392acdec0380324afb31ab8
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2026-04-27 21:23:41 +0200

    getterek & setterek #5

--- DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/JwtPropertyConfiguration.java
+++ DrivingLicenseAppAPI/src/main/java/csapat/DrivingLicenseAppAPI/config/security/JWT/JwtPropertyConfiguration.java
@@ -7,10 +7,32 @@ import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "jwt")
 @Component
-@Getter
-@Setter
 public class JwtPropertyConfiguration {
     private String secret;
     private Long expiredTime;
     private String issuer;
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public Long getExpiredTime() {
+        return expiredTime;
+    }
+
+    public void setExpiredTime(Long expiredTime) {
+        this.expiredTime = expiredTime;
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
 }
