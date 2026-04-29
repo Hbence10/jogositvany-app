6727cb14001719e9ee74d59629e362d7af3c13a1
Biró Szabolcs Benjámin <70518815+BiroSzabolcsBenjamin@users.noreply.github.com>
2026-04-29 20:19:26 +0200

    Update app.routes.server.ts

--- driving_licence_app/src/app/app.routes.server.ts
+++ driving_licence_app/src/app/app.routes.server.ts
@@ -2,6 +2,37 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'searchPage/:type',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { type: "instructor" },
+        { type: "school" }
+      ]
+    },
+  },
+  {
+    path: 'request/:owner',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { type: "instructor" },
+        { type: "school" }
+      ]
+    },
+  },
+  {
+    path: 'profil/:type/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { type: "user", id: '1' },
+        { type: "school", id: '1' }
+      ]
+    },
+  },
+
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
